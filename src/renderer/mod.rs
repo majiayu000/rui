56207a66259c7f781825bba53312e0f711cb75d7
majiayu000 <1835304752@qq.com>
2026-05-27 15:12:00 +0800

    Add renderer primitive support contract
    Refs #47

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -10,12 +10,14 @@ mod text_shaping;
 #[cfg(target_os = "macos")]
 pub mod metal;
 
-pub use primitives::Primitive;
+pub use primitives::{PathVertex, Primitive, PrimitiveKind};
 pub use resources::{
     GlyphResourceKey, ImageResourceEntry, ImageResourceKey, RendererDeviceDiagnostics,
     RendererDiagnostics, RendererImageCache, RendererResourceAllocation, RendererResourceCache,
     RendererResourceError, RendererResourceHandle, RendererResourceId, RendererResourceKind,
-    RendererResourceStats,
+    RendererResourceStats, RendererUnsupportedPrimitive,
 };
-pub use runtime::{RecordedScene, RecordingRenderer, Renderer, RendererError};
+pub use runtime::{
+    RecordedScene, RecordingRenderer, Renderer, RendererError, RendererPrimitiveSupport,
+};
 pub use scene::{HitRegion, Layer, LayerId, Scene, ZIndex};
